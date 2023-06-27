7bdd60f2d8ea86a815b85e4c7dcc7bc7db7cab70
Emiko Esther S <graceupongraceemi@gmail.com>
2023-06-27 13:06:01 -0700

    Home page.jsx updated

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,8 +5,18 @@ export const metadata = {
   description: 'Discover & Share AI Prompts'
 };
 
-const layout = () => {
-  return <div>layout</div>;
+const RootLayout = ({ children }) => {
+  return (
+    <html lang='en'>
+      <body>
+        <div className='main'>
+          <div className='gradient' />
+        </div>
+
+        <main className='app'>{children}</main>
+      </body>
+    </html>
+  );
 };
 
-export default layout;
+export default RootLayout;
