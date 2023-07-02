4ce503e20d4106739038264fbcfee7af3923a5f7
Emiko Esther S <graceupongraceemi@gmail.com>
2023-07-02 06:55:01 -0700

    database.js done with MongoDB and route.js updated

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,6 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+import { connectToDB } from '@utils/database';
 
 // console.log({
 //   clientId: process.env.GOOGLE_ID,
@@ -17,6 +18,11 @@ const handler = NextAuth({
   async signIn({ profile }) {
     try {
       // serverless -> Lambda -> dynamodb
+      await connectToDB();
+
+      // check if a user already exits
+
+      // it not, create a new user
     } catch (error) {}
   }
 });
