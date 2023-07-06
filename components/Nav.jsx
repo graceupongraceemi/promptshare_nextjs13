5e85fef1391825a5c51c7d3112972a72223e4a1f
Emiko Esther S <graceupongraceemi@gmail.com>
2023-07-06 00:05:06 -0700

    src changed to session user image in av.jsx

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -25,7 +25,7 @@ const Nav = () => {
     <nav className='flex-between w-full mb-16 pt-3'>
       <Link href='/' className='flex gap-2 flex-center'>
         <Image
-          src='/assets/images/logo.svg'
+          src={session?.user.image}
           alt='Promptopia Logo'
           width={30}
           height={30}
@@ -48,7 +48,7 @@ const Nav = () => {
 
             <Link href='/profile'>
               <Image
-                src='/assets/images/logo.svg'
+                src={session?.user.image}
                 width={37}
                 height={37}
                 className='rounded-full'
@@ -78,7 +78,7 @@ const Nav = () => {
         {session?.user ? (
           <div className='flex'>
             <Image
-              src='/assets/images/logo.svg'
+              src={session?.user.image}
               width={37}
               height={37}
               className='rounded-full'
